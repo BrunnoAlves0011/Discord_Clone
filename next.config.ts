b32c454d5a5e2d8bd6f5545c1a901cd2253fb445
Brunno Alves <brunno3dgamer@gmail.com>
2025-07-03 15:03:18 -0300

    First Release Discord Clone

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "uploadthing.com",
+      "gwlxcbjf2z.ufs.sh"
+    ]
+  }
+};
+
+export default nextConfig;
